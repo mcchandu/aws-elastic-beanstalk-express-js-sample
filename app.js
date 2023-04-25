d9406e9b84e1fced6fd3499bb1ef2865276eb16e
Aniket Sharma <aniketsharma8800@gmail.com>
2023-04-26 00:30:35 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hey There, This is my Project CICD pipeline using AWS.git'));
+app.get('/', (req, res) => res.send('Hey There, my project is done I am now tesing it'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
